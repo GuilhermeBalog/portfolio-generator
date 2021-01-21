8699cedae8835e92d41b946995b7d6caf90dc0aa
GuilhermeBalog <GuilhermeBalog@users.noreply.github.com>
2021-01-21 04:47:22 +0000

    deploy: 94f3458501616a91b7825c35ef913c552d7b4745

---
+++ _next/static/89BN11dqbVte-B8-OfQd3/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F4eab5eb4d5182602beb4.css","static\u002Fchunks\u002Fpages\u002Findex-4b761deef7afda7349b1.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-bb10018a5733dd8fec35.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- _next/static/mX1IlO6ytlzN87AxqpHn4/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F4eab5eb4d5182602beb4.css","static\u002Fchunks\u002Fpages\u002Findex-6c900cd231eb319ee7a7.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-bb10018a5733dd8fec35.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
