185f2ca127c16422308e3f4b7b7628b136fecf55
GuilhermeBalog <GuilhermeBalog@users.noreply.github.com>
2021-03-16 18:00:13 +0000

    deploy: 34f5bfdd9589b85f625e4928d73d38c2ac8ac599

---
+++ _next/static/TJxP_iQA0d97JK-AH9udx/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F53d7967009557280a874.css","static\u002Fchunks\u002Fpages\u002Findex-53570f37b2403b639474.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-ad600b451fbb200d3a16.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
